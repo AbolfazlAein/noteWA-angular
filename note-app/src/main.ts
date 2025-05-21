9a1014fee7ebe173a2d80774deec00c11f3e386f
AbolfazlAein <156539794+AbolfazlAein@users.noreply.github.com>
2025-05-21 20:20:49 +0330

    modify bootstrap

--- note-app/src/main.ts
+++ note-app/src/main.ts
@@ -1,6 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { NotesComponent } from './app/notes/notes.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(NotesComponent)
+  .catch(err => console.error(err));
