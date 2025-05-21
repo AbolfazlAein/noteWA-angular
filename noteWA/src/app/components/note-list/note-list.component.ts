f0f52e3dae2c822bc2ca331d399a4f25e49e8deb
AbolfazlAein <156539794+AbolfazlAein@users.noreply.github.com>
2025-05-21 13:16:32 +0330

    Implement note-list components(html,css,ts)

--- noteWA/src/app/components/note-list/note-list.component.ts
+++ noteWA/src/app/components/note-list/note-list.component.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Note } from '../../models/note';
+import { NoteService } from '../../services/note.service';
 
 @Component({
   selector: 'app-note-list',
-  imports: [],
   templateUrl: './note-list.component.html',
-  styleUrl: './note-list.component.css'
+  styleUrls: ['./note-list.component.scss']
 })
-export class NoteListComponent {
+export class NoteListComponent implements OnInit {
+  notes: Note[] = [];
+  selectedNote: Note | null = null;
 
+  constructor(private noteService: NoteService) { }
+
+  ngOnInit(): void {
+    this.loadNotes();
+  }
+
+  loadNotes(): void {
+    this.notes = this.noteService.getNotes();
+  }
+
+  createNewNote(): void {
+    const newNote = this.noteService.createNote();
+    this.notes = [newNote, ...this.notes];
+    this.noteService.saveNotes(this.notes);
+    this.selectedNote = newNote;
+  }
+
+  selectNote(note: Note): void {
+    this.selectedNote = note;
+  }
+
+  saveNote(updatedNote: Note): void {
+    this.notes = this.noteService.updateNote(updatedNote.id, updatedNote);
+    this.selectedNote = null;
+  }
+
+  backToList(): void {
+    this.selectedNote = null;
+  }
 }
